2c1b6cb28a414a544d4acf73af62b6899cd88249
tus <tuskung8030@gmail.com>
2020-11-10 17:05:22 +0700

    merge bike master

--- src/FastMoveConsoleApplication.java
+++ src/FastMoveConsoleApplication.java
@@ -1,3 +1,7 @@
+
+import data.ToyData;
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +13,11 @@
  * @author praisan
  */
 public class FastMoveConsoleApplication {
+    Scanner sc = new Scanner(System.in);
+    public void addBike(){
+        String licensePlate = sc.nextLine();
+        int engineCapacity = sc.nextInt();
+        
+    }
     
 }
